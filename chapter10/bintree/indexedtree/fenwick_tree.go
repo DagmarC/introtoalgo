ec794d69f27b44f76d0265fc62aa12c6cc876c4b
DagmarMac <dg,cervenkova@gmail.com>
2024-02-23 13:56:30 +0100

    fenwick tree

---
+++ chapter10/bintree/indexedtree/fenwick_tree.go
@@ -0,0 +1,64 @@
+package indexedtree
+
+import "fmt"
+
+type FenwickTree struct {
+	size    int   // n: length of the array
+	arr     []int // array: the input array on which queries are made
+	bitTree []int // bit: store the sum of range
+}
+
+// The computation of   $g(i)$  is defined using the following simple operation:
+// we replace all trailing  $1$  bits in the binary representation of  $i$  with  $0$  bits.
+// g(11) = g2(1011) = 1000 = 8
+// g(12) = g2(1100) = 1100 = 12
+// g(13) = g2(1101) = 1100 = 12
+// g(15) = g2(1111) = 0000 = 0
+func NewFenwickTree(arr []int) *FenwickTree {
+	ft := FenwickTree{
+		size:    len(arr) + 1,
+		arr:     arr,
+		bitTree: make([]int, len(arr)+1),
+	}
+	ft.bitTree = append([]int{-100}, arr...) // copy original elements from arr to bitTree array, ignore 1st element
+
+	for i := 1; i < ft.size; i++ {
+		ft.addDirectParent(i, ft.bitTree[i])
+	}
+	return &ft
+}
+
+// Sum of elements from array in range [0, r]
+// indexed from 1, arr [1,2,3,4] i=3 -> elements 1, 2, 3
+// 7 	= 00111
+// -7	= 11001 /2s complement
+// 00111 & 11001 = 00001
+// 00111 - 00001 = 00110 "child"
+func (ft *FenwickTree) Sum(i int) int {
+	sum := 0
+	for i > 0 {
+		sum += ft.bitTree[i]
+		i -= (i & -i) // flip the last set bit
+	}
+	return sum
+}
+
+// Add the value in the array on index i with increment inc
+// 7 	= 00111
+// -7	= 11001 /2s complement
+// 00111 & 11001 = 00001
+// 00111 + 00001 = 01000 PARENT IDX
+func (ft *FenwickTree) Add(i, inc int) {
+	fmt.Printf("ADD: i=%d\tinc=%d\t\tft.size=%d\n", i, inc, ft.size)
+	for i <= ft.size {
+		ft.bitTree[i] += inc
+		i += (i & -i) // add last set bit
+	}
+}
+
+func (ft *FenwickTree) addDirectParent(i, inc int) {
+	parent := i + (i & -i)
+	if parent < ft.size {
+		ft.bitTree[parent] = ft.bitTree[parent] + ft.bitTree[i]
+	}
+}
---
+++ chapter10/bintree/indexedtree/fenwick_tree_test.go
@@ -0,0 +1,99 @@
+package indexedtree
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestBuildTree(t *testing.T) {
+	tests := []struct {
+		arr      []int
+		want []int
+	}{
+		{
+			arr:      []int{1, 2, 3, 4, 5, 6, 7, 8},
+			want: []int{-100, 1, 3, 3, 10, 5, 11, 7, 36},
+		},
+	}
+	for _, tc := range tests {
+		ft := NewFenwickTree(tc.arr)
+		if !cmp.Equal(ft.bitTree, tc.want) {
+			t.Errorf("got %v, want %v", ft.bitTree, tc.want)
+		}
+	}
+}
+
+func TestSum(t *testing.T) {
+	tests := []struct {
+		ft   FenwickTree
+		i    int
+		want int
+	}{
+		{
+			ft:   FenwickTree{size: 8, arr: []int{1, 2, 3, 4, 5, 6, 7, 8}, bitTree: []int{-100, 1, 3, 3, 10, 5, 11, 7, 36}},
+			i:    3,
+			want: 6,
+		},
+		{
+			ft:   FenwickTree{size: 8, arr: []int{1, 2, 3, 4, 5, 6, 7, 8}, bitTree: []int{-100, 1, 3, 3, 10, 5, 11, 7, 36}},
+			i:    4,
+			want: 10,
+		},
+		{
+			ft:   FenwickTree{size: 8, arr: []int{1, 2, 3, 4, 5, 6, 7, 8}, bitTree: []int{-100, 1, 3, 3, 10, 5, 11, 7, 36}},
+			i:    8,
+			want: 36,
+		},
+		{
+			ft:   FenwickTree{size: 8, arr: []int{1, 2, 3, 4, 5, 6, 7, 8}, bitTree: []int{-100, 1, 3, 3, 10, 5, 11, 7, 36}},
+			i:    5,
+			want: 15,
+		},
+	}
+	for _, tc := range tests {
+		if got := tc.ft.Sum(tc.i); got != tc.want {
+			t.Errorf("got %v, want %v", got, tc.want)
+		}
+	}
+}
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		ft   FenwickTree
+		i    int
+		inc  int
+		want []int
+	}{
+		{
+			ft:   FenwickTree{size: 8, arr: []int{1, 2, 3, 4, 5, 6, 7, 8}, bitTree: []int{-100, 1, 3, 3, 10, 5, 11, 7, 36}},
+			i:    3,
+			inc:  3,
+			want: []int{-100, 1, 3, 6, 13, 5, 11, 7, 39},
+		},
+		{
+			ft:   FenwickTree{size: 8, arr: []int{1, 2, 3, 4, 5, 6, 7, 8}, bitTree: []int{-100, 1, 3, 3, 10, 5, 11, 7, 36}},
+			i:    1,
+			inc:  10,
+			want: []int{-100, 11, 13, 3, 20, 5, 11, 7, 46},
+		},
+		{
+			ft:   FenwickTree{size: 8, arr: []int{1, 2, 3, 4, 5, 6, 7, 8}, bitTree: []int{-100, 1, 3, 3, 10, 5, 11, 7, 36}},
+			i:    8,
+			inc:  4,
+			want: []int{-100, 1, 3, 3, 10, 5, 11, 7, 40},
+		},
+		{
+			ft:   FenwickTree{size: 8, arr: []int{1, 2, 3, 4, 5, 6, 7, 8}, bitTree: []int{-100, 1, 3, 3, 10, 5, 11, 7, 36}},
+			i:    7,
+			inc:  3,
+			want: []int{-100, 1, 3, 3, 10, 5, 11, 10, 39},
+		},
+	}
+	for _, tc := range tests {
+		tc.ft.Add(tc.i, tc.inc)
+		if !cmp.Equal(tc.ft.bitTree, tc.want) {
+			t.Errorf("got %v, want %v", tc.ft.bitTree, tc.want)
+		}
+	}
+}
